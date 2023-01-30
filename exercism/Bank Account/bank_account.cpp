31ecf9d96e9beb9f677adb87fb4223c6657df247
AdamTempest <jj030469@gmail.com>
2023-01-30 21:35:08 +0630

    declared methods outside class

--- exercism/Bank Account/bank_account.cpp
+++ exercism/Bank Account/bank_account.cpp
@@ -1,61 +1,55 @@
 #include "bank_account.h"
 
 namespace Bankaccount {
-	class Bankaccount {
-	public:
-		void open(){
-			if (!opened){
-				opened = true;
-			} else {
-				throw std::invalid_argument("Sorry, you cannot open this account as it is already opened.");
-			}
+	void Bankaccount::open(){
+		if (!opened){
+			opened = true;
+		} else {
+			throw std::invalid_argument("Sorry, you cannot open this account as it is already opened.");
 		}
-		
-		int balance(){
-			if (opened){
-				return user_balance;
-			} else {
-				throw std::invalid_argument("Sorry, the balance cannot be checked. Your account is closed.");
-			}
+	}
+	
+	int Bankaccount::balance(){
+		if (opened){
+			return user_balance;
+		} else {
+			throw std::invalid_argument("Sorry, the balance cannot be checked. Your account is closed.");
 		}
+	}
 
-		void deposit(int amount){
-			if (opened){
-				if (amount>0){
-					user_balance+=amount;
-				} else{
-					throw std::invalid_argument("Sorry, you cannot deposit negative amount.");
-				}
-			} else {
-				throw std::invalid_argument("Sorry, you cannot deposit into this account. It is closed.");
-			}
-		}
-
-		void withdraw(int amount){
-			if (opened){
-				if (amount < 0){
-					throw std::invalid_argument("Sorry, you cannot withdraw negative amount.");
-				} else if (amount <= user_balance){
-					user_balance -= amount;
-				} else {
-					throw std::invalid_argument("Sorry, that amount exceeds your balance.");
-				}
+	void Bankaccount::deposit(int amount){
+		if (opened){
+			if (amount>0){
+				user_balance+=amount;
 			} else{
-				throw std::invalid_argument("Sorry, you cannot withdraw from this account. It is closed.");
+				throw std::invalid_argument("Sorry, you cannot deposit negative amount.");
 			}
+		} else {
+			throw std::invalid_argument("Sorry, you cannot deposit into this account. It is closed.");
 		}
+	}
 
-		void close(){
-			if (opened){
-				user_balance = 0;
-				opened = false;
-			} else{
-				throw std::invalid_argument("Sorry, you cannot close this account since it is not opened.");
+	void Bankaccount::withdraw(int amount){
+		if (opened){
+			if (amount < 0){
+				throw std::invalid_argument("Sorry, you cannot withdraw negative amount.");
+			} else if (amount <= user_balance){
+				user_balance -= amount;
+			} else {
+				throw std::invalid_argument("Sorry, that amount exceeds your balance.");
 			}
+		} else{
+			throw std::invalid_argument("Sorry, you cannot withdraw from this account. It is closed.");
 		}
+	}
 
-	private:
-		bool opened = false;
-		int user_balance = 0;
-	};
+	void Bankaccount::close(){
+		if (opened){
+			user_balance = 0;
+			opened = false;
+		} else{
+			throw std::invalid_argument("Sorry, you cannot close this account since it is not opened.");
+		}
+	}
+
 }
